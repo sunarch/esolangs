0d28ff939f7ee03d35d5a20ed735e6ea36ca089a
András Németh <sunarch@protonmail.com>
2023-07-12 21:18:06 +0200

    Change version files to C headers

---
+++ sunarch/el-montaje/src/version.h
@@ -0,0 +1,13 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef EL_MONTAJE_VERSION_H
+#define EL_MONTAJE_VERSION_H
+
+#define  PROGRAM_NAME     "EL montaje"
+#define  PROGRAM_VERSION  "0.1.0"
+
+#endif // ndef EL_MONTAJE_VERSION_H
---
+++ sunarch/gridrun/src/version.h
@@ -0,0 +1,13 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef GRIDRUN_VERSION_H
+#define GRIDRUN_VERSION_H
+
+#define  PROGRAM_NAME     "GridRun"
+#define  PROGRAM_VERSION  "0.1.0"
+
+#endif // ndef GRIDRUN_VERSION_H
